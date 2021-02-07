00336e6edcda5b41c6ade2467becf0fc25a2fdbf
madfive <madfive22@gmail.com>
2021-02-07 16:01:20 +0100

    Initial Commit

---
+++ Geofence/GeofenceApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeofenceApp.swift
+//  Geofence
+//
+//  Created by MadFive on 2021. 02. 07..
+//
+
+import SwiftUI
+
+@main
+struct GeofenceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
